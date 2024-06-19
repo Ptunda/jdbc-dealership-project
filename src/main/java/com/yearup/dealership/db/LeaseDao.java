2b06cd579a51b276002f362d0b3a1f63e9dbe3ce
Ptunda <ptunda@pgh.yearup.org>
2024-06-18 23:37:42 -0400

    Implement the logic to add a sales contract

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class LeaseDao {
+
     private DataSource dataSource;
 
     public LeaseDao(DataSource dataSource) {
@@ -19,18 +20,19 @@ public class LeaseDao {
 
     public void addLeaseContract(LeaseContract leaseContract) {
 
-        
-        String insertLeaseContractSQL = "INSERT INTO lease_contracts (VIN, lease_start, lease_end, monthly_payment)\n" +
-                                        "VALUES (?, ?, ?, ?)";
+
+        String insertLeaseContractSQL = "INSERT INTO lease_contracts (contract_id, VIN, lease_start, lease_end, monthly_payment)\n" +
+                                        "VALUES (?, ?, ?, ?, ?)";
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement insertLeaseContractPreparedStatement = connection.prepareStatement(insertLeaseContractSQL)
         ) {
 
-            insertLeaseContractPreparedStatement.setString(1, leaseContract.getVin());
-            insertLeaseContractPreparedStatement.setDate(2, Date.valueOf(leaseContract.getLeaseStart()));
-            insertLeaseContractPreparedStatement.setDate(3, Date.valueOf(leaseContract.getLeaseEnd()));
-            insertLeaseContractPreparedStatement.setDouble(4, leaseContract.getMonthlyPayment());
+            insertLeaseContractPreparedStatement.setInt(1, leaseContract.getContractId());
+            insertLeaseContractPreparedStatement.setString(2, leaseContract.getVin());
+            insertLeaseContractPreparedStatement.setDate(3, Date.valueOf(leaseContract.getLeaseStart()));
+            insertLeaseContractPreparedStatement.setDate(4, Date.valueOf(leaseContract.getLeaseEnd()));
+            insertLeaseContractPreparedStatement.setDouble(5, leaseContract.getMonthlyPayment());
 
             insertLeaseContractPreparedStatement.executeUpdate();
 
--- src/main/java/com/yearup/dealership/db/SalesDao.java
+++ src/main/java/com/yearup/dealership/db/SalesDao.java
@@ -4,17 +4,43 @@ import com.yearup.dealership.models.SalesContract;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class SalesDao {
+
     private DataSource dataSource;
 
     public SalesDao(DataSource dataSource) {
+
         this.dataSource = dataSource;
+
     }
 
     public void addSalesContract(SalesContract salesContract) {
-        // TODO: Implement the logic to add a sales contract
+
+        
+        String insertSalesContractSQL = "INSERT INTO sales_contracts (contract_id, VIN, sales_date, price)\n" +
+                                        "VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement insertLeaseContractPreparedStatement = connection.prepareStatement(insertSalesContractSQL)
+        ) {
+
+            insertLeaseContractPreparedStatement.setInt(1, salesContract.getContractId());
+            insertLeaseContractPreparedStatement.setString(2, salesContract.getVin());
+            insertLeaseContractPreparedStatement.setDate(3, Date.valueOf(salesContract.getSaleDate()));
+            insertLeaseContractPreparedStatement.setDouble(4, salesContract.getPrice());
+
+            insertLeaseContractPreparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            System.out.println("Error adding lease contract " + e.getMessage());
+
+        }
+
     }
+
 }
