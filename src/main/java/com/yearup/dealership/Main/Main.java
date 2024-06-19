88d16686a649bedee7f70626e33d171b33e853a7
Ptunda <ptunda@pgh.yearup.org>
2024-06-18 21:42:33 -0400

    Added a feature in the main method to check if we have the program arguments

--- src/main/java/com/yearup/dealership/Main/Main.java
+++ src/main/java/com/yearup/dealership/Main/Main.java
@@ -19,6 +19,14 @@ import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
+
+        if (args.length != 2){
+
+            System.out.println("Username and password arguments are required!");
+            System.exit(1);
+
+        }
+        
         String username = args[0];
         String password = args[1];
 
